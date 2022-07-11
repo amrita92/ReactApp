1dc461e9966867076a81dad07c44989a5a66302e
Amrita Iyer <iyeramrita8@gmail.com>
2022-07-10 20:04:38 -0700

    changes in app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is a perfect marriage of javascript and markup.
         </a>
       </header>
     </div>
